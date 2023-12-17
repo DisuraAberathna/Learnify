841a575b24721bd3575172e39dbbabfadc5cc2e1
Disura Chathwara <disura2005@gmail.com>
2023-12-17 21:34:10 +0530

    Footer created

---
+++ components/footer.php
@@ -0,0 +1,34 @@
+<footer class="col-10 rounded-3 mb-3" style="background-color: #B5F1FD;">
+    <div class="row text-center py-2">
+        <div class="col">
+            <button class="p-0 m-0 footer-btn">
+                <img src="./resources/img/footer_icon_1.svg" alt="footer_icon_1" width="25px" />
+                <p>Dashboard</p>
+            </button>
+        </div>
+        <div class="col">
+            <button class="p-0 m-0 footer-btn">
+                <img src="./resources/img/footer_icon_2.svg" alt="footer_icon_1" width="25px" />
+                <p>Notification</p>
+            </button>
+        </div>
+        <div class="col">
+            <button class="p-0 m-0 footer-btn">
+                <img src="./resources/img/footer_icon_3.svg" alt="footer_icon_1" width="25px" />
+                <p>Profile</p>
+            </button>
+        </div>
+        <div class="col">
+            <button class="p-0 m-0 footer-btn">
+                <img src="./resources/img/footer_icon_4.svg" alt="footer_icon_1" width="25px" />
+                <p>My Classes</p>
+            </button>
+        </div>
+        <div class="col">
+            <button class="p-0 m-0 footer-btn">
+                <img src="./resources/img/footer_icon_5.svg" alt="footer_icon_1" width="25px" />
+                <p>Store</p>
+            </button>
+        </div>
+    </div>
+</footer>
--- index.php
+++ index.php
@@ -1,1 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Document</title>
+
+    <link rel="stylesheet" href="./css/style.css" />
+    <link rel="stylesheet" href="./css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css" />
+</head>
+
+<body>
+
+    <div class="container-xxl">
+        <div class="row">
+
+            <div class="col-12 d-flex justify-content-center fixed-bottom">
+                <?php include "./components/footer.php"; ?>
+            </div>
+        </div>
+    </div>
+
+    <script src="./js/bootstrap.bundle.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+</body>
+
+</html>
